07036a112e87a84e3ec3cd482b354a61ab63f870
Suneelar <Suneelar>
2025-10-22 14:54:45 +0530

    commit

---
+++ Array/Elem.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Elem {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array elements");
+        for (int i = 0; i <= sc.nextInt(); i++) {
+        //    System.out.println(nums[i]); 
+        System.out.println(i);
+        }
+        
+    }
+}
