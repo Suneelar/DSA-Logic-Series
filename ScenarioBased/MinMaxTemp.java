11a0f694f15720deec1c3e28b9c6e14c5d91e932
Suneelar <suneelar397@gmail.com>
2025-11-05 23:03:34 +0530

    Update solutions

---
+++ ScenarioBased/MinMaxTemp.java
@@ -0,0 +1,20 @@
+package ScenarioBased;
+class Temperature{
+private double min;
+private double max;
+public double getMin() {
+    return min;
+}
+public double getMax() {
+    return max;
+}
+
+}
+
+ 
+
+public class MinMaxTemp {
+    public static void main(String[] args) {
+        
+    }
+}
