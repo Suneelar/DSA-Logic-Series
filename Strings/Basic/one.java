7f59be72b25a00ea10b76f99b589fb01be1bdc09
Suneelar <suneelar397@gmail.com>
2025-12-05 22:49:19 +0530

    updated solutions

---
+++ Strings/Basic/one.java
@@ -0,0 +1,10 @@
+package Strings.Basic;
+
+public class one {
+    public static void main(String[] args) {
+       String str="Java Exercises!";
+       char ch[]=str.toCharArray();
+       System.out.println(ch[0]);
+       System.out.println(ch[10]); 
+    }
+}
